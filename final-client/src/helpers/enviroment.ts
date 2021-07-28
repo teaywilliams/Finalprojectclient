df1320dce7338e0b4030943bc667cfc0d76e8c10
Teayarnna Williams <teaywilliams@Teayarnnas-MBP.hsd1.fl.comcast.net>
2021-07-28 19:17:34 -0400

    Heroku app addition

--- final-client/src/helpers/enviroment.ts
+++ final-client/src/helpers/enviroment.ts
@@ -1,10 +1,12 @@
 let APIURL = "";
 
 switch (window.location.hostname) {
-  case "localhost":
-  case "127.0.0.1":
+  case "localhost" || "127.0.0.1":
     APIURL = "http://localhost:8080";
     break;
+    case "fortheloveofbrie.herokuapp.com":
+      APIURL = "https://fortheloveofbrie-server.herokuapp.com";
+      break
 }
 
 export default APIURL;
